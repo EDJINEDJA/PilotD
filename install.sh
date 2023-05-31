474b936f9691189b3ff28be7ebd7285a2a06c3a0
EDJINEDJA <edjinedjalaris@gmail.com>
2023-05-31 16:23:17 +0200

    Code clean #(00005)

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!bin/bash
-
+set -o errexit
 echo "update the terminal"
 sudo apt-get update
 
 echo "upgrade the terminal"
 sudo apt-get upgrade
+
+echo "apdate pip"
+pip install --upgrade
 
 echo "instaling dependancies"
 pip install -r requirements.txt
